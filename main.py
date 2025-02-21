479d34e33a571dbffbdef0a2f2b9d6db55227772
dmitrijsuhanov713@gmail.com <dmitrijsuhanov713@gmail.com>
2025-02-21 19:55:27 +0300

    установка соединения с бд

--- main.py
+++ main.py
@@ -199,4 +199,6 @@ def delete_habit(habit_id):
 
 
 def update_habit_name(habit_id, new_name):
+    # Устанавливаем соединение с базой данных 'habits.db'.
+    conn = sqlite3.connect('habits.db')
 #endregion
