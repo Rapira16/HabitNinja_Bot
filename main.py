3f75ef3a9dd6c24be7bdbbe4d38f57a118ac820a
dmitrijsuhanov713@gmail.com <dmitrijsuhanov713@gmail.com>
2025-02-21 14:40:26 +0300

    Добавил все необходимые импорты в проект

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import telebot
+import sqlite3
+from datetime import datetime
+import schedule
+import time
+import threading
+import random
+from telebot.types import (
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    ReplyKeyboardRemove
+)
