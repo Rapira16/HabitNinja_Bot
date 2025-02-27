aeaa4779541bed7eaa964c250c42f1f1a53d2dd3
n1kpavlov <nikniknik388@gmail.com>
2025-02-27 14:13:10 +0300

    Изменил визуальное оформление

--- main.py
+++ main.py
@@ -182,13 +182,13 @@ def update_habit_name(habit_id, new_name):
 # region Menu & Handlers
 def create_menu():
     menu = ReplyKeyboardMarkup(resize_keyboard=True)
-    menu.add(KeyboardButton("Добавить привычку"))
-    menu.add(KeyboardButton("Отметить выполнение"))
-    menu.add(KeyboardButton("Статистика"))
-    menu.add(KeyboardButton("Удалить привычку"))
-    menu.add(KeyboardButton("Редактировать привычку"))
-    menu.add(KeyboardButton("Установить напоминание"))
-    menu.add(KeyboardButton("Установить мотивационное сообщение"))
+    menu.add(KeyboardButton("Добавить привычку ➕"))
+    menu.add(KeyboardButton("Отметить выполнение ✅"))
+    menu.add(KeyboardButton("Статистика 📊"))
+    menu.add(KeyboardButton("Удалить привычку ❌"))
+    menu.add(KeyboardButton("Редактировать привычку ✏️"))
+    menu.add(KeyboardButton("Установить напоминание ⏰"))
+    menu.add(KeyboardButton("Установить мотивационное сообщение ⏰"))
     return menu
 
 # endregion
